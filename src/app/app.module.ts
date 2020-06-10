a0ee64f37329a0b8c1df1580802f0095b14883c7
ocervantes <omarck.cervantes@gmail.com>
2020-06-09 20:54:42 -0500

    controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ProductComponent } from './components/menu/product/product.component';
+import { CustomerComponent } from './components/menu/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servicess/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+  env: any = environment;
+
+  getProduct()): Observable<any> {
+    return this.http.get(`${this.env.getProduct}`);
+  }
+}
